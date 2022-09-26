6412197d9cac39238522e677296f0475fd735f56
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-09-26 14:56:49 +0700

    Add project files.

---
+++ BulkyBook.DataAccess/Migrations/20220907153516_addProductToDb.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    public partial class addProductToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_CoverTypes_CoverTypeId1",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CoverTypeId1",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CoverTypeId1",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CoverTypeId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CoverTypeId",
+                table: "Products",
+                column: "CoverTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_CoverTypes_CoverTypeId",
+                table: "Products",
+                column: "CoverTypeId",
+                principalTable: "CoverTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_CoverTypes_CoverTypeId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CoverTypeId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoverTypeId",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CoverTypeId1",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CoverTypeId1",
+                table: "Products",
+                column: "CoverTypeId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_CoverTypes_CoverTypeId1",
+                table: "Products",
+                column: "CoverTypeId1",
+                principalTable: "CoverTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# BulkyBook
+![image](https://user-images.githubusercontent.com/94780400/152375628-75ff2391-b5e5-43ab-86a6-60daeb61b472.png)
+![image](https://user-images.githubusercontent.com/94780400/152472249-982aaa84-80ae-40b2-b42c-80fd71580dc8.png)
+![image](https://user-images.githubusercontent.com/94780400/152472643-e915ab86-642b-4309-b7ba-8e942255f85a.png)
+![image](https://user-images.githubusercontent.com/94780400/152551562-23586394-74c7-4a18-b6ca-1f64a3229bc0.png)
+![image](https://user-images.githubusercontent.com/94780400/152551607-17e6975c-0530-48cd-820c-968d0ff4f792.png)
+![image](https://user-images.githubusercontent.com/94780400/152646043-3bd56f36-fa5c-41f5-aa7d-78184a904bb7.png)
+![image](https://user-images.githubusercontent.com/94780400/152646706-6b318c91-4171-4de8-a93f-57ce12be77fb.png)
+![image](https://user-images.githubusercontent.com/94780400/152647020-065aded1-1af4-47a9-9923-fa621dfaa987.png)
+![image](https://user-images.githubusercontent.com/94780400/152647860-cc48af94-ca6d-458d-a277-63cb1214b5d4.png)
+![image](https://user-images.githubusercontent.com/94780400/152688803-db104f61-2bf3-4993-a37c-dea717c7898f.png)
+![image](https://user-images.githubusercontent.com/94780400/152688809-ebbe6eb8-2462-4dcd-8e91-297556f4a423.png)
+![image](https://user-images.githubusercontent.com/94780400/152916012-1c6c5c8b-a7b1-4a4c-b3b8-39c1475d0a0c.png)
+![image](https://user-images.githubusercontent.com/94780400/153004482-5a037533-e389-47f0-aa7f-9febb26880a2.png)
+<h1>https://bulkybookwebv1406.azurewebsites.net/
+The website is under development and created by me
+</h1>
+
+
+
+
+
+
+
+<h2>https://bootswatch.com/solar/</h2>
+<h2>https://getbootstrap.com/docs/5.0/getting-started/introduction/</h2>
+<h2>https://www.dotnetmastery.com/Home/Vlog</h2>
